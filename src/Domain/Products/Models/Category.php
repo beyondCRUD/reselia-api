2b48b0cde6881e0c3ae13026ef1d46fcfc220cd1
rama <rama@javan.co.id>
2023-12-09 12:21:03 +0700

    seeders

---
+++ src/Domain/Products/Models/Category.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Domain\Products\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use HasFactory, HasUlids, SoftDeletes;
+
+    protected $fillable = ['id', 'title', 'parent_id'];
+
+    public function childs(): HasMany
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
+}
